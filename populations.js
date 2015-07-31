3cb71f25e6e0447b2e8d620664b69a2ad23973a5
VisualVortex <toolive@list.ru>
2015-07-31 15:00:20 +0300

    end

--- populations.js
+++ populations.js
@@ -8,7 +8,7 @@ function getData(url, callback) {
     var RESPONSES = {
         '/countries': [
             {name: 'Cameroon', continent: 'Africa'},
-            {name :'Fiji Islands', continent: 'Oceania'},
+            {name: 'Fiji Islands', continent: 'Oceania'},
             {name: 'Guatemala', continent: 'North America'},
             {name: 'Japan', continent: 'Asia'},
             {name: 'Yugoslavia', continent: 'Europe'},
@@ -45,73 +45,10 @@ function getData(url, callback) {
 /**
  * Ваши изменения ниже
  */
-    /*
-     * ! Для начала я бы давал переменным более понятные названия.
-     *
-     * */
 var requests = ['/countries', '/cities', '/populations'];
-var responses = {};
-var rearranged_responses = {};
+var responses = {}, rebuilt_responses = {};
 
-var get_population_by_input = function(input) {
-    var res = undefined;
-
-    if (input in rearranged_responses['populations']) res = get_city_population(input);
-    else if (input in rearranged_responses['cities']) res = get_country_population(input);
-    else if (input in rearranged_responses['countries']) res = get_area_population(input);
-    return res
-};
-
-var get_area_population = function(area) {
-    var countries = rearranged_responses['countries'][area];
-
-    var res = undefined;
-    if (countries) res = countries.reduce(function(sum, city) {
-        return sum + get_country_population(city);
-    }, 0);
-    return res;
-};
-
-var get_country_population = function(country) {
-    var cities = rearranged_responses['cities'][country];
-
-    var res = undefined;
-    if (cities) res = cities.reduce(function(sum, city) {
-        return sum + get_city_population(city);
-    }, 0);
-    return res;
-};
-
-var get_city_population = function(city) {
-    return rearranged_responses['populations'][city];
-};
-
-var rearrange_populations = function(dicts) {
-    var res = {};
-    dicts.forEach(function(dict) {
-        res[dict['name']] = dict['count'];
-    });
-    return res;
-};
-
-var rearrange_cities_or_countries = function(dicts) {
-    var keys = Object.keys(dicts[0]);
-
-    var first_key = 'name';
-    var second_key = keys[0] === first_key ? keys[1] : keys[0];
-
-    var res = {};
-    dicts.forEach(function(dict) {
-        var first_key_val = dict[first_key];
-        var second_key_val = dict[second_key];
-
-        if (second_key_val in res) res[second_key_val].push(first_key_val);
-        else res[second_key_val] = [first_key_val];
-    });
-    return res;
-};
-
-requests.forEach(function(request) {
+requests.forEach(function (request) {
     var callback = function (error, result) {
 
         if (error) {
@@ -122,23 +59,85 @@ requests.forEach(function(request) {
 
         if (Object.keys(responses).length == requests.length) {
 
-            rearranged_responses['populations'] = rearrange_populations(responses['/populations']);
-            rearranged_responses['cities'] = rearrange_cities_or_countries(responses['/cities']);
-            rearranged_responses['countries'] = rearrange_cities_or_countries(responses['/countries']);
+            rebuilt_responses['populations'] = rebuild_populations(responses['/populations']);
+            rebuilt_responses['cities'] = rebuild_cities_or_countries(responses['/cities']);
+            rebuilt_responses['countries'] = rebuild_cities_or_countries(responses['/countries']);
 
-            var answer = 'Total population in African cities: ' + get_area_population('Africa');
+            var answer = 'Total population in African cities: ' + get_continent_population('Africa');
             console.log(answer);
-
-            var input = prompt('Specify name of city, country or area', 'Africa');
-            answer = 'Total population in ' + input + ': ' + get_population_by_input(input);
-            if (get_population_by_input(input) !== undefined) {
-                console.log(answer);
+            var input = prompt('Please write name of city, country or continent', 'Africa');
+            if (input === null) {
+                console.log("Input cancelled by User");
             }
-            else
-                console.log("Sorry. We have no Data about this place.");
+            else {
+                answer = 'Total population in ' + input + ': ' + get_population_by_input(input);
+                if (get_population_by_input(input) !== undefined) {
+                    console.log(answer);
+                }
+                else
+                    console.log("Sorry. We have no Data about this place.");
+            }
         }
     };
 
     getData(request, callback);
 
 });
+
+var get_population_by_input = function (input) {
+    var res = undefined;
+
+    if (input in rebuilt_responses['populations']) res = get_city_population(input);
+    else if (input in rebuilt_responses['cities']) res = get_country_population(input);
+    else if (input in rebuilt_responses['countries']) res = get_continent_population(input);
+    return res
+};
+
+var get_continent_population = function (continent) {
+    var countries = rebuilt_responses['countries'][continent];
+
+    var res = undefined;
+    if (countries) res = countries.reduce(function (sum, city) {
+        return sum + get_country_population(city);
+    }, 0);
+    return res;
+};
+
+var get_country_population = function (country) {
+    var cities = rebuilt_responses['cities'][country];
+
+    var res = undefined;
+    if (cities) res = cities.reduce(function (sum, city) {
+        return sum + get_city_population(city);
+    }, 0);
+    return res;
+};
+
+var get_city_population = function (city) {
+    return rebuilt_responses['populations'][city];
+};
+
+var rebuild_populations = function (libs) {
+    var res = {};
+    libs.forEach(function (lib) {
+        res[lib['name']] = lib['count'];
+    });
+    return res;
+};
+
+var rebuild_cities_or_countries = function (libs) {
+    var keys = Object.keys(libs[0]);
+
+    var first_key = 'name';
+    var second_key = keys[0] === first_key ? keys[1] : keys[0];
+
+    var res = {};
+    libs.forEach(function (lib) {
+        var first_key_val = lib[first_key];
+        var second_key_val = lib[second_key];
+
+        if (second_key_val in res) res[second_key_val].push(first_key_val);
+        else res[second_key_val] = [first_key_val];
+    });
+    return res;
+};
